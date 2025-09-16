11b5a80dc7ece51c0a58864e839a63fcf8d1c1df
danibl-314 <danibeltran0121@gmail.com>
2025-09-15 21:32:11 -0500

    Agregando mi primer cambio

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1068c6188197032e9ba5ec00aa7c0304d5c90c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f15bebdee787dd917fb297496e524e55459f4e49")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
